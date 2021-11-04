990b8bbb4cca4841e89a358d1ffa3762362974cb
ghsoares <ghsoares99795@gmail.com>
2021-11-04 18:46:25 -0300

    changed api to stardard version instead of mono

--- src/gdlibrary.cpp
+++ src/gdlibrary.cpp
@@ -1,5 +1,39 @@
 #include "reactgd.h"
 
+godot_pluginscript_language_desc lps_language_desc = {
+    .name = "GDX",
+    .type = "GDX",
+    .extension = "gdx",
+    .recognized_extensions = (const char *[]){"gdx", NULL},
+    .init = &lps_language_init,
+    .finish = &lps_language_finish,
+    .reserved_words = (const char *[]){
+        "and", "break", "do", "else", "elseif", "end",
+        "false", "for", "function", "goto", "if", "in",
+        "local", "nil", "not", "or", "repeat", "return",
+        "then", "true", "until", "while",
+        "self", "_G", "_ENV", "_VERSION",
+        NULL},
+    .comment_delimiters = (const char *[]){"--", "--[[ ]]", NULL},
+    .string_delimiters = (const char *[]){"' '", "\" \"", "[[ ]]", "[=[ ]=]", NULL},
+    .has_named_classes = false,
+    .supports_builtin_mode = false,
+
+    .add_global_constant = &lps_language_add_global_constant,
+    .script_desc = {
+        .init = &lps_script_init,
+        .finish = &lps_script_finish,
+        .instance_desc = {
+            .init = &lps_instance_init,
+            .finish = &lps_instance_finish,
+            .set_prop = &lps_instance_set_prop,
+            .get_prop = &lps_instance_get_prop,
+            .call_method = &lps_instance_call_method,
+            .notification = &lps_instance_notification,
+        },
+    },
+};
+
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o)
 {
     godot::Godot::gdnative_init(o);
@@ -8,6 +42,11 @@ extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o)
 extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o)
 {
     godot::Godot::gdnative_terminate(o);
+}
+
+extern "C" void GDN_EXPORT godot_gdnative_singleton(void *handle) {
+    godot_pluginscript_register_language(&lps_language_desc);
+    godot::Godot::print("Hello world!");
 }
 
 extern "C" void GDN_EXPORT godot_nativescript_init(void *handle)
--- src/reactgd.h
+++ src/reactgd.h
@@ -1,7 +1,17 @@
 #ifndef REACTGD_H
 #define REACTGD_H
-
 #include <Godot.hpp>
-#include <Sprite.hpp>
+
+godot_pluginscript_language_data *lps_language_init();
+void lps_language_finish(godot_pluginscript_language_data *data);
+void lps_language_add_global_constant(godot_pluginscript_language_data *data, const godot_string *name, const godot_variant *value);
+godot_pluginscript_script_manifest lps_script_init(godot_pluginscript_language_data *data, const godot_string *path, const godot_string *source, godot_error *error);
+void lps_script_finish(godot_pluginscript_script_data *data);
+godot_pluginscript_instance_data *lps_instance_init(godot_pluginscript_script_data *data, godot_object *owner);
+void lps_instance_finish(godot_pluginscript_instance_data *data);
+godot_bool lps_instance_set_prop(godot_pluginscript_instance_data *data, const godot_string *name, const godot_variant *value);
+godot_bool lps_instance_get_prop(godot_pluginscript_instance_data *data, const godot_string *name, godot_variant *ret);
+godot_variant lps_instance_call_method(godot_pluginscript_instance_data *data, const godot_string_name *method, const godot_variant **args, int argcount, godot_variant_call_error *error);
+void lps_instance_notification(godot_pluginscript_instance_data *data, int notification);
 
 #endif
